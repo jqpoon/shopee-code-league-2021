701717fa262bd298edec9059b9677af2382947f6
Matthew Booker <mattbooker97@gmail.com>
2021-03-06 15:50:43 +0800

    Changed user structure and added initial logic for search

--- shopee.py
+++ shopee.py
@@ -1,25 +1,66 @@
 import pandas
+import time
 df = pandas.read_json("contacts.json")
 
 
 class User:
-    email = None
-    id = None
-    phone = None
-    total_contacts = 0
 
-    def __init__(self, email = None, id_num = None, phone = None):
+    def __init__(self, idx, email, phone, order_id, contacts):
         self.email = email
-        self.id = id_num
+        self.idx = idx
         self.phone = phone
+        self.order_id = order_id
+        self.total_contacts = contacts
 
     def __repr__(self):
         return f"{self.email}, {self.id}, {self.phone}, {self.total_contacts}"
-        
-    def merge():
-        pass
 
 
-# email_dict = dict.fromkeys(df["Email"].tolist())
-# order_dict = dict.fromkeys(df["Id"].tolist())
-# phone_dict = dict.fromkeys(df["Phone"].tolist())
+email_dict = {}
+id_dict = {}
+phone_dict = {}
+
+
+def merge(user, index, email, phone, order_id, contacts):
+    pass
+
+def create_new_user(index, email, phone, order_id, contacts):
+    new_user = User(index, email, phone, order_id, contacts)
+
+    if new_user.email != "":
+        email_dict[new_user.email] = new_user
+
+    if new_user.order_id != "":
+        id_dict[new_user.order_id] = new_user
+    
+    if new_user.phone != "":
+        phone_dict[new_user.phone] = new_user
+
+
+s = time.time()
+
+for index,row in df.iterrows():
+
+    email = row["Email"]
+    phone = row["Phone"]
+    order_id = row["OrderId"]
+    contacts = row["Contacts"]
+    user_exists = False
+
+    if email != "" and email in email_dict:
+        merge(email_dict[email], index, email, phone, order_id, contacts)
+        user_exists = True
+    
+    if order_id != "" and order_id in id_dict:
+        merge(id_dict[order_id], index, email, phone, order_id, contacts)
+        user_exists = True
+
+    if phone != "" and phone in phone_dict:
+        merge(phone_dict[phone], index, email, phone, order_id, contacts)
+        user_exists = True
+
+    if not user_exists:
+        create_new_user(index, email, phone, order_id, contacts)
+
+
+print(time.time() - s)
